ef33e93652adf292c12ea4d1913a76407cc1d7eb
LeonardoPeron <leonardo.peron@lb2.com.br>
2021-03-05 21:36:03 -0300

    tudo nosso

--- src/main/java/br/puc/ed/arrays/Arrays.java
+++ src/main/java/br/puc/ed/arrays/Arrays.java
@@ -6,13 +6,13 @@ import br.puc.ed.game.RegistroPontuacao;
 public class Arrays {
 
   public static void main(String[] args) {
-    int[] intArray;
-    intArray = new int[10];
-    intArray[0] = 56;
-    intArray[1] = 34;
-    for(int i = 0; i < intArray.length; i++) {
-      System.out.println(i + ": " + intArray[i]);
-    }
+    // int[] intArray;
+    // intArray = new int[10];
+    // intArray[0] = 56;
+    // intArray[1] = 34;
+    // for(int i = 0; i < intArray.length; i++) {
+    //   System.out.println(i + ": " + intArray[i]);
+    // }
 
     RegistroPontuacao rp = new RegistroPontuacao("Leonardo", 100);
 
