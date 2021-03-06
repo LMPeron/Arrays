0e9e9d2b1d2a8c42d34cefb4bd46adb1abf8544b
LeonardoPeron <leonardo.peron@lb2.com.br>
2021-03-05 21:15:32 -0300

    nois prof

--- src/main/java/br/puc/ed/game/PainelPontuacao.java
+++ src/main/java/br/puc/ed/game/PainelPontuacao.java
@@ -3,17 +3,30 @@ package br.puc.ed.game;
 public class PainelPontuacao {
   private RegistroPontuacao[] registros;
   private int quantidadeRegistros;
+  private int capacidade;
 
-  public PainelPontuacao(int capacidade){
+  public PainelPontuacao(int capacidade) {
     registros = new RegistroPontuacao[capacidade];
     quantidadeRegistros = 0;
+    this.capacidade = capacidade;
   }
 
-  public void adicionar(RegistroPontuacao rp){
-
+  public void adicionar(RegistroPontuacao rp) {
+    registros[quantidadeRegistros] = rp;
   }
 
-  public void imprimir(){
-
+  public void imprimir() {
+    for (int i=0; i<=quantidadeRegistros; i++) {
+      System.out.println(registros[i]);
+    }
   }
+
+  public void limpar() {
+    this.registros = new RegistroPontuacao[this.capacidade];
+  }
+
+  public void excluirAleatorio() {
+    
+  }
+
 }
